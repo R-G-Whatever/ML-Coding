391ca27107ff7f940bbe5491c6e1ee2caa014253
zhuqing <zhuqing.zhang0422@gmail.com>
2025-04-11 21:39:05 -0700

    Update: lession 2: logistic regression

--- ml_coding/logistic_regression.py
+++ ml_coding/logistic_regression.py
@@ -1,6 +1,8 @@
 import numpy as np
+
+
 class LogisticRegression:
-    def __init__(self, lr= 0.01, num_iter = 100, tol = 1e-6):
+    def __init__(self, lr=0.01, num_iter=100, tol=1e-6):
         self.lr = lr
         self.num_iter = num_iter
         self.tol = tol
@@ -8,33 +10,73 @@ class LogisticRegression:
         self.bias = None
 
     def sigmoid(self, z):
+        # pay attention when divide something
+        # can 1 + np.exp(-Z) close to 0?
         return 1 / (1 + np.exp(-z))
 
     def init_para(self, n_features):
+        # Does initialition matters here? --- NO because the loss function logistic regression is convex
+        # When convex it is guarantee to have a solution then it will have a closed form
         self.w = np.zeros(n_features)
 
-    def predict_prod(self, X):
+    def predict_prob(self, X):
+        # X shape (row, num_features)
+        # self.w shape (num_features, 1)
+        # X @ self.w
         return self.sigmoid(np.dot(X, self.w) + self.bias)
 
-    def predict(self, X, threshold = 0.5):
-        proba = self.predict_prod(X)
+    def predict(self, X, threshold=0.5):
+        proba = self.predict_prob(X)
         return (proba >= threshold).astype(int)
 
     def fit(self, X, y):
+        # fit function gradient descent
         n_samples, n_features = X.shape
 
         self.init_para(n_features)
 
         for i in range(self.num_iter):
-            model = np.dot(X, self.w) + self.bias
+            predict = self.sigmoid(np.dot(X, self.w) + self.bias)
 
-            predict = self.sigmoid(model)
+            dw = (
+                np.dot(X.T, (predict - y)) / n_samples
+            )  # based on derivative of the loss function
+            db = (
+                np.sum(predict - y) / n_samples
+            )  # based on derivative of the loss function
 
-            dw = np.dot(X.T, (predict - y)) / n_samples
-            db = np.sum(predict - y) / n_samples
-
+            # Homework 2: derive function for logistic regression
+            # Step1: sigmoid derivative
+            # Step2: loss function - chain rule for derivative
             self.w -= self.lr * dw
             self.bias -= self.lr * db
 
             if np.linalg.norm(dw) < self.tol and abs(db) < self.tol:
                 break
+                # [1,2,3,4] --- L2 norm = np.sqrt(1**2 + 2**2 + 3**2 + 4**2) ---> scaler
+                #           --- L1 norm = (abs(1) + abs(2) + abs(3) + abs(4))
+
+            # homework 3 --- Use closed form to calculate the LR results
+
+
+# General traditional ML class structure
+# class LogisticRegression:
+#     def __init__(self, lr = 0.01, num_iter = 500, tol = 1e-5):
+#         self.lr = lr
+#         self.num_iter = num_iter
+#         self.tol = tol
+#
+#     def fit(self, X, y):
+#         return
+#
+#     def predict(self, X):
+#         return y
+
+
+if __name__ == "__main__":
+    lr = LogisticRegression()
+    print(lr.bias)
+
+    # K4 - pay extra attention over float problem when divide by close to zero
+    A = 99.9e-1000
+    print(1 / A)
